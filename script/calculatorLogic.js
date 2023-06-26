5e4af82b2ff73068d471051383a804183fdec49f
Andres Felipe M <andrefe91@gmail.com>
2023-06-26 18:41:31 -0500

    Another part of the logic for the calculator function, like equal, AC and Delete

--- script/calculatorLogic.js
+++ script/calculatorLogic.js
@@ -5,6 +5,8 @@ let operationState = false;
 let buttons = document.querySelectorAll('.numbers button');
 let buttonClear = document.querySelector('.stateActions #btnAC');
 let buttonEqual = document.querySelector('.stateActions #btnEqual');
+let buttonDel = document.querySelector('.stateActions #btnDel');
+let buttonBlock = document.querySelectorAll('.numbers .number');
 
 let operationDisplayText = document.querySelector('.operationDisplay');
 let historyDisplayText = document.querySelector('.history');
@@ -13,6 +15,7 @@ let historyDisplayText = document.querySelector('.history');
 buttons.forEach( (button) => button.addEventListener('click',getButtonText));
 buttonClear.addEventListener('click',clear);
 buttonEqual.addEventListener('click',equal);
+buttonDel.addEventListener('click',del);
 
 
 function getButtonText (button) {
@@ -48,6 +51,7 @@ function printOperationDisplay(key) {
         operationDisplayText.textContent = 
         operationDisplayText.textContent.concat(' ',key, ' ');
         operationState = true;
+        enableButton();
     } else {
         if (!zerostate) {
             operationDisplayText.textContent = key;
@@ -56,6 +60,9 @@ function printOperationDisplay(key) {
             operationDisplayText.textContent.concat('',key);
         }
         
+        if (operationDisplayText.textContent.length>9) {
+            blockButton();
+        }
     }
     zerostate = true;
 
@@ -86,21 +93,33 @@ function equal(){
     let lastNumber = Number(query[2]);
     let result = 0;
 
-    /*Calculatin withn the four basic operations */
-    switch(operation){
-        case '+':
-            result = sum(firstNumber,lastNumber);
-            break;
-        case '-':
-            result = substract(firstNumber,lastNumber);
-            break;
-        case 'x':
-            result = multiply(firstNumber,lastNumber);
-            break;
-        case '/':
-            result = div(firstNumber,lastNumber);
-            break;
+    
+    /*Main Logic*/
+    /*First, make sure that the variables are numbers*/
+    if(!isNaN(firstNumber) && isNaN(lastNumber)){
+        result = firstNumber;
+    } else if(isNaN(firstNumber)){
+        result = 0;
+    } else {
+        switch(operation){
+            /*Calculating with the four basic operations */
+            case '+':
+                result = sum(firstNumber,lastNumber);
+                break;
+            case '-':
+                result = substract(firstNumber,lastNumber);
+                break;
+            case 'x':
+                result = multiply(firstNumber,lastNumber);
+                break;
+            case '/':
+                result = div(firstNumber,lastNumber);
+                break;
+        }
     }
+
+    /*Rounding up the number to six decimal places*/
+    result = +(result.toFixed(8));
 
     /*Must update the history and operation display*/
     historyDisplayText.textContent = operationDisplayText.textContent;
@@ -129,4 +148,41 @@ function div (a,b) {
         return 'Nop!!';
     }
     return a/b;
+}
+
+function del() {
+
+    let ext = -1;
+
+    /*Delete the caracters in the string*/
+    if (operationDisplayText.textContent.length == 1){
+        operationDisplayText.textContent = 0;
+        zerostate = false;
+    }else {
+        /*Block to delete the space around along the operand*/
+        if (operationDisplayText.textContent.slice(-1) == ' ') {
+            ext = -3;
+        } else {
+            /*Do Nothing*/
+        }
+        operationDisplayText.textContent = operationDisplayText.textContent.slice(0,ext);
+        console.log('Remaining: ' + Array.from(operationDisplayText.textContent));
+    }
+
+}
+
+function blockButton() {
+
+    /*Blocking the numbers for a given condition*/
+    buttonBlock.forEach(button => {
+        button.removeEventListener('click',getButtonText);
+    });
+}
+
+function enableButton() {
+
+    /*Unblocking the numbers for a given condition*/
+    buttonBlock.forEach(button => {
+        button.addEventListener('click',getButtonText);
+    });
 }
